6123270d6bd1b91d0efd69cbb856a1b1a6d3952b
nawedita <naweditakeshari@gmail.com>
2023-04-02 16:23:19 +0530

    space added

--- src/components/company-search/CompanyContent.jsx
+++ src/components/company-search/CompanyContent.jsx
@@ -134,7 +134,7 @@ const CompanyContent = () => {
 
                     <div className="container-fluid">
                         <div className="d-sm-flex align-items-center justify-content-between mb-4">
-                            <h1 className="h3 mb-0 text-gray-800">Search Companies</h1>
+                            <h1 className="h3 mb-0 text-gray-800">Search Companies </h1>
                             <a href="#" className="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
                                 className="fas fa-download fa-sm text-white-50"></i> Generate Report</a>                    </div>
                         <div className="row">
