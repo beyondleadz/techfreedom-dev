1a6f04a959bd0cddbb8cae8d79d5f35e48fdc875
jayanti <naweditakeshari@gmail.com>
2023-04-12 15:19:06 +0530

    images and content updation

--- src/components/securitypolicy/Policy.jsx
+++ src/components/securitypolicy/Policy.jsx
@@ -6,12 +6,82 @@ const Policy=()=>{
     <>
 <div className="container">
 		<div className="privacy-security-term">
-		<p>Beyond Leadz is the flagship product of ABIA (Advanced Business Intelligence and Analytics Pvt. Ltd.), which was formed by a team of professionals experienced in Business Intelligence, Research and Analytics. Our Board of advisors consists of globally acknowledged thought leaders with an established background in Business Intelligence, transformation and innovation.</p>
-		
-<h5>Customer Oriented Company</h5>
+		<h3 align="center"><strong>Beyond Leadz Security Policy</strong></h3><br/>
 
+<p>When registering with BeyondLeadz, you can be assured of the implementation of all industry standard security practices. We take your online security concerns seriously and continuously strive to provide an improved customer experience. BeyondLeadz ensures the security of the product and your information, at all times, with the Trustworthy Computing approach. Under the Trustworthy Computing approach, multi- level security is maintained, such as the Physical Layers and Logical Layers. All in all, protection is given over physical controls, access control and video surveillance. Data isolation, hosted application security, single sign on, identity and access management is also taken care of.
+</p>		
 
-<p>Beyond Leadz is the flagship product of ABIA (Advanced Business Intelligence and Analytics Pvt. Ltd.), which was formed by a team of professionals experienced in Business Intelligence, Research and Analytics. Our Board of advisors consists of globally acknowledged thought leaders with an established background in Business Intelligence, transformation and innovation.</p>
+<h5>PHYSICAL SECURITY</h5>
+<p>To maintain the security of information and data stored by customers online, BeyondLeadz ensures and maintains several security checks in the physical domain of the stored data. Following are a few practices that help BeyondLeadz ensure all round security for its users:
+</p>
+
+<p>
+<ul type="disc">
+  	  <li style={{marginLeft:50}}><strong> •	Authorized Personnel Access:</strong> Physical access to the data environment is limited to authorized personnel only. This access is given to an individual only after verifying the need and eligible requirement, such as to manage customers’ applications and services. Further, a few more security processes such as double authentication for physical access, round the clock video surveillance and on premise security officers secures the data center environment.</li>	  
+      <li style={{marginLeft:50}}><strong> •	Physical Monitoring:</strong> Over the limitation of physical access, the data center is further secured with motion sensors, 100 percent- secured access protected with security alarms and video surveillance.</li>
+      <li style={{marginLeft:50}}><strong> •	Disaster Risk Management:</strong> Provision to sustain through seismic disasters and also well equipped for prevention of fire break outs.</li>
+      <li style={{marginLeft:50}}><strong> •	Climate Control and Measurement:</strong> Constant and continuous measurement of humidity, temperature is carried out to ensure smooth running of the equipment.
+</li>
+      <li style={{marginLeft:50}}><strong> •	Secure Network Design:</strong> The BeyondLeadz network is designed in a way which ensures necessary segmentation of critical servers and public facing interfaces.</li>
+      <li style={{marginLeft:50}}><strong> •	Enhanced Hardware:</strong> The hardware that is used at BeyondLeadz data center works in the most efficient and secure manner, keeping in mind the security practices followed by BeyondLeadz. This equipment’s efficiency helps the company save power and costs, the benefits of which are then extended to our esteemed customers.
+</li>
+
+</ul>
+</p>
+
+<h5>LOGICAL SECURITY</h5>					
+<p>Logical Security plays an equally important role at BeyondLeadz as Physical Security. The following practices followed will give an over- all picture of the logical security level maintained by BeyondLeadz for its customers.
+</p>
+
+<p>
+<ul type="disc">
+  	  <li style={{marginLeft:50}}><strong> •	Data Segregation/ Isolation:</strong> It is ensured at all times that the multiple customer data is segregated and is allowed access to the respective customer only. Data isolation and various logically induced security boundaries, also help BeyondLeadz in maintaining a separate database for each customer without giving access to or being compromised by any other user.</li>	  
+
+      <li style={{marginLeft:50}}><strong> •	Hosted Application Security:</strong> BeyondLeadz ensures that the applications hosted by its data center are monitored under strict security controls and measures, at all times. The following features enable BeyondLeadz maintain the required security:
+</li>
+
+<p>
+<li style={{marginLeft:50}}><strong> •	Customizable Security Roles:</strong> BeyondLeadz controls the user access and the activities that can be performed by them.</li>
+<li style={{marginLeft:50}}><strong> •	Role Based Forms:</strong> Manages the visibility of data as per each individual’s role.
+</li>
+<li style={{marginLeft:50}}><strong> •	Data Encryption:</strong> To maintain confidentiality of data, all customer information in the database is encrypted by strong encryption algorithms.</li>
+<li style={{marginLeft:50}}><strong> •	Field-Level Security:</strong> As per the field level security settings, administrators can restrict users’ access to view and edit specific fields.
+</li>
+<li style={{marginLeft:50}}><strong> •	Group Creation and Management:</strong> Private Groups can be created and managed to keep data within a certain group of members.
+</li>
+<li style={{marginLeft:50}}><strong> •	Subscriber Management:</strong> System subscribers are controlled and managed to ensure segregation of organizational data at all times.
+</li>
+<li style={{marginLeft:50}}><strong> •	User Management:</strong> Separate profiles with different levels of access rights are created to maintain User Access and to ensure optimum use of the product.
+</li>
+<li style={{marginLeft:50}}><strong> •	User Access Management:</strong> Easy and user-friendly interface is established to manage the logged in users for any organization which has subscribed to BeyondLeadz.
+</li>
+</p>
+
+      <li style={{marginLeft:50}}><strong> •	Infrastructure level Security:</strong> A multi - tier administration model which separates administrative activities and access controls as per the user role and level of authorized administrative access.
+</li>
+      <li style={{marginLeft:50}}><strong> •	Network Level Security:</strong> Internet connections established between customers and BeyondLeadz data centers are encrypted with the help of TLS and SSL. By doing this, the data is given complete security and confidentiality between the customer internet location and data center. BeyondLeadz’s network security practices also ensure 99.9 percent network availability.
+</li>
+<li style={{marginLeft:50}}><strong> •	Identity and Access Management:</strong> Following are the methods that control access to the systems hosting BeyondLeadz.
+</li>
+
+<p>
+<li style={{marginLeft:50}}><strong> •	Staff Level Access Control:</strong> To safeguard the customers of BeyondLeadz and their assets/ information, an access control policy has been established. This access privilege is granted based on authority, duty and identity verification.</li>
+<li style={{marginLeft:50}}><strong> •	Pro- active Host Security:</strong> BeyondLeadz’s online security is enhanced by proactively securing the host system.</li>
+  <p>
+    <li style={{marginLeft:50}}> •	Server hardening by disabling unnecessary service</li>
+    <li style={{marginLeft:50}}> •	Logging and auditing</li>
+  </p>
+<li style={{marginLeft:50}}><strong> •	Restricted access to service:</strong></li>
+  <p>
+    <li style={{marginLeft:50}}> •	Content inspection</li>
+    <li style={{marginLeft:50}}> •	Hardened servers</li>
+    <li style={{marginLeft:50}}> •	Sessions protected by SSL/TLS</li>
+  </p>
+</p>
+
+</ul>
+</p>
+
 		</div>
 		</div>
 	
