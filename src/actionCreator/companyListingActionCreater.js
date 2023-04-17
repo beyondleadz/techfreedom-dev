9ebf574519e5a97b3f2e6e936cf51660fe165ba3
unknown <naweditakeshari@gmail.com>
2023-04-17 23:53:59 +0530

    demmy data added for industrylist

--- src/actionCreator/companyListingActionCreater.js
+++ src/actionCreator/companyListingActionCreater.js
@@ -5,15 +5,22 @@ import {getAuthMethod} from '../services/HttpServices';
 import { industryApiUrl } from "../constant/Constant";
 
 export const getIndustryList = (payload) =>(dispatch)=> {
-  return getAuthMethod(industryApiUrl).then(res=>{
-    console.log(res.data)
-    dispatch(
-      {
-        type: INDUSTRY_LIST,
-        payload: res.data,
-      }
-    )
+  // return getAuthMethod(industryApiUrl).then(res=>{
+  //   console.log(res.data)
+  //   dispatch(
+  //     {
+  //       type: INDUSTRY_LIST,
+  //       payload: res.data,
+  //     }
+  //   )
     
-  })
+  // }
+  return dispatch(
+    {
+      type: INDUSTRY_LIST,
+      payload: [{id:1,name:"Animation"},{id:2,name:"Accounting"}],
+    }
+  )
+  //)
   
 };
