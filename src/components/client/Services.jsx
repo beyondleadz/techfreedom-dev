e5095aced9a16cb2be9c3a68c94059a824467635
jayanti <naweditakeshari@gmail.com>
2023-04-17 20:11:01 +0530

    img and content addition

--- src/components/client/Services.jsx
+++ src/components/client/Services.jsx
@@ -1,14 +1,27 @@
 import React,{useState} from "react";
 import { useRoutes,NavLink } from "react-router-dom";
 
-import PanasonicImage from '../../assets/images/client-logo-panasonic.jpg';
-import MNC22Image from  '../../assets/images/mnc22.png';
-import MNC33Image from '../../assets/images/mnc33.png';
-import MNC44Image from  '../../assets/images/mnc44.png';
-import MNC55Image from '../../assets/images/mnc55.png';
-import MNC66Image from  '../../assets/images/mnc661.png';
-import MNC88Image from  '../../assets/images/mnc88.png';				
-import MNC77Image from  '../../assets/images/mnc77.png';				
+import PanasonicImage from '../../assets/images/client-logo-panasonic.png';
+import MakeMyTripImage from  '../../assets/images/client-logo-make-my-trip.png';
+import SodexoImage from '../../assets/images/client-logo-sodexo.png';
+import TataImage from  '../../assets/images/client-logo-tata.png';
+import EyImage from '../../assets/images/client-logo-ey.png';
+import PeaceChannelImage from  '../../assets/images/client-logo-peace-channel.png';
+import CanonImage from  '../../assets/images/client-logo-canon.png';				
+import BMWImage from  '../../assets/images/client-logo-bmw.png';				
+import MahindraRiseImage from  '../../assets/images/client-logo-mahindra-rise.png';	
+import AirtelImage from  '../../assets/images/client-logo-airtel.png';				
+import ITCImage from  '../../assets/images/client-logo-itc.png';				
+import IIMAhamadabadImage from  '../../assets/images/client-logo-iim-ahamadabad.png';				
+import LemonTreeHotelImage from  '../../assets/images/client-logo-lemon-tree-hotel.png';				
+import WurthImage from  '../../assets/images/client-logo-wurth.png';				
+import DellImage from  '../../assets/images/client-logo-dell.png';				
+import SimenseImage from  '../../assets/images/client-logo-simense.png';				
+import CarrefourImage from  '../../assets/images/client-logo-carrefour.png';				
+import BimtechImage from  '../../assets/images/client-logo-bimtech.png';				
+import RamcoImage from  '../../assets/images/client-logo-ramco.png';				
+// import Image from  '../../assets/images/client-logo-.png';				
+
 
 const Services=()=>{
     return (
@@ -18,60 +31,130 @@ const Services=()=>{
 			<div className="container">
 				<h3 className="title-style-clients mb-2">Some of our satisfied clients</h3>
 				<div className="row agile-choose-grids">
+
+				<div className="col-sm-3 agile-choose-grid" position-relative>
+						<div className="choose-icon">
+						<a href="#" className="client-logo"><img src={PeaceChannelImage} className="img-responsive" alt="client" /></a>
+						</div>						
+					</div>
+
+					<div className="col-sm-3 agile-choose-grid">
+						<div className="choose-icon">
+						<a href="#" className="client-logo"><img src={CanonImage} className="img-responsive" alt="client" /></a>
+						</div>
+					</div>
+
+					<div className="col-sm-3 agile-choose-grid">
+						<div className="choose-icon">
+						<a href="#" className="client-logo"><img src={EyImage} className="img-responsive" alt="client" /></a>
+						</div>
+					</div>
+
+					<div className="col-sm-3 agile-choose-grid">
+						<div className="choose-icon">
+						<a href="#" className="client-logo"><img src={MakeMyTripImage} className="img-responsive" alt="client" /></a>
+						</div>
+					</div>
+
+
 					<div className="col-sm-3 agile-choose-grid" position-relative>
+						<div className="choose-icon">
+						<a href="#" className="client-logo"><img src={BMWImage} className="img-responsive" alt="client" /></a>
+						</div>
+					</div>
+
+					<div className="col-sm-3 agile-choose-grid">
 						<div className="choose-icon">
 						<a href="#" className="client-logo"><img src={PanasonicImage} className="img-responsive" alt="client" /></a>
-						</div>
-						
-					</div>
-					<div className="col-sm-3 agile-choose-grid">
-						<div className="choose-icon">
-						<a href="#" className="client-logo"><img src={MNC22Image} className="img-responsive" alt="client" /></a>
-						</div>
-						
-					</div>
-					<div className="col-sm-3 agile-choose-grid">
-						<div className="choose-icon">
-						<a href="#" className="client-logo"><img src={MNC33Image} className="img-responsive" alt="client" /></a>
-						</div>
-						
+						</div>						
 					</div>
 					
 					<div className="col-sm-3 agile-choose-grid">
 						<div className="choose-icon">
-						<a href="#" className="client-logo"><img src={MNC44Image} className="img-responsive" alt="client" /></a>
+						<a href="#" className="client-logo"><img src={MahindraRiseImage} className="img-responsive" alt="client" /></a>
 						</div>
-						
+					</div>					
+					
+					<div className="col-sm-3 agile-choose-grid">
+						<div className="choose-icon">
+						<a href="#" className="client-logo"><img src={AirtelImage} className="img-responsive" alt="client" /></a>
+						</div>						
 					</div>
-					
-					
+
 					<div className="col-sm-3 agile-choose-grid" position-relative>
 						<div className="choose-icon">
-						<a href="#" className="client-logo"><img src={MNC55Image} className="img-responsive" alt="client" /></a>
+						<a href="#" className="client-logo"><img src={ITCImage} className="img-responsive" alt="client" /></a>
 						</div>
-						
+					</div>
+
+					<div className="col-sm-3 agile-choose-grid">
+						<div className="choose-icon">
+						<a href="#" className="client-logo"><img src={IIMAhamadabadImage} className="img-responsive" alt="client" /></a>
+						</div>
 					</div>
 					
 					<div className="col-sm-3 agile-choose-grid">
 						<div className="choose-icon">
-						<a href="#" className="client-logo"><img src={MNC66Image} className="img-responsive" alt="client" /></a>
+						<a href="#" className="client-logo"><img src={LemonTreeHotelImage} className="img-responsive" alt="client" /></a>
 						</div>
-						
+					</div>					
+					
+					<div className="col-sm-3 agile-choose-grid">
+						<div className="choose-icon">
+						<a href="#" className="client-logo"><img src={WurthImage} className="img-responsive" alt="client" /></a>
+						</div>						
+					</div>
+
+					<div className="col-sm-3 agile-choose-grid" position-relative>
+						<div className="choose-icon">
+						<a href="#" className="client-logo"><img src={DellImage} className="img-responsive" alt="client" /></a>
+						</div>
+					</div>
+
+					<div className="col-sm-3 agile-choose-grid">
+						<div className="choose-icon">
+						<a href="#" className="client-logo"><img src={SimenseImage} className="img-responsive" alt="client" /></a>
+						</div>
 					</div>
 					
 					<div className="col-sm-3 agile-choose-grid">
 						<div className="choose-icon">
-						<a href="#" className="client-logo"><img src={MNC88Image} className="img-responsive" alt="client" /></a>
+						<a href="#" className="client-logo"><img src={TataImage} className="img-responsive" alt="client" /></a>
 						</div>
-						
+					</div>	
+
+					<div className="col-sm-3 agile-choose-grid">
+						<div className="choose-icon">
+						<a href="#" className="client-logo"><img src={SodexoImage} className="img-responsive" alt="client" /></a>
+						</div>
+					</div>
+
+
+					<div className="col-sm-3 agile-choose-grid" position-relative>
+						<div className="choose-icon">
+						<a href="#" className="client-logo"><img src={CarrefourImage} className="img-responsive" alt="client" /></a>
+						</div>						
 					</div>
 					
 					<div className="col-sm-3 agile-choose-grid">
 						<div className="choose-icon">
-						<a href="#" className="client-logo"><img src={MNC77Image} className="img-responsive" alt="client" /></a>
+						<a href="#" className="client-logo"><img src={BimtechImage} className="img-responsive" alt="client" /></a>
 						</div>
-						
+					</div>					
+					
+					<div className="col-sm-3 agile-choose-grid">
+						<div className="choose-icon">
+						<a href="#" className="client-logo"><img src={RamcoImage} className="img-responsive" alt="client" /></a>
+						</div>
 					</div>
+					
+					<div className="col-sm-3 agile-choose-grid">
+						<div className="choose-icon">
+						<a href="#" className="client-logo"><img src={RamcoImage} className="img-responsive" alt="client" /></a>
+						</div>
+					</div>
+					
+					
 					
 				</div>	
 			</div>
