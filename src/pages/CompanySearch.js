4ff875666641f104b36424957226303e679ae41b
unknown <naweditakeshari@gmail.com>
2023-04-24 23:37:34 +0530

    page design

--- src/pages/CompanySearch.js
+++ src/pages/CompanySearch.js
@@ -15,12 +15,101 @@ const CompanySearch = () => {
       key: "1",
       label: <span > <i className="fa tab-img la la-building"></i>Companies</span>,
       children: (
-        <div id="wrapper">
+        <div>
+              <nav className="navbar navbar-light bg-white topbar mb-4 static-top">
+            <div className="buttons-container m-mt quickselection">
+           
+              <div>              
+                <span className="fs-12 mr-2">Quick Selection</span>
+                <div className=" fs-12 d-sm-inline-block mr-1">
+                  <input type="text" className="quickselectioninput" />
+                </div>
+                <span className=" fs-12 mr-1">to</span>
+                <div className="  fs-12 d-sm-inline-block mr-1">
+                  <input type="text" className="quickselectioninput" />
+                </div>
+                <button className=" fs-12 d-sm-inline-block btn btn-info btn-lg mr-1">
+                  <i className="fas fs-12 fa-arrow-right"></i>{" "}
+                </button>
+                <button className="fs-12  btn btn-info btn-lg  mr-3">
+                  Select All
+                </button>
+              </div>
+              <ul className="flex  m-mt">
+                <li>
+                  <a
+                    className=" mr-2"
+                    href="#"
+                    id=""
+                    role="button"
+                    data-toggle=""
+                    aria-haspopup="true"
+                    aria-expanded="false"
+                  >
+                    <i
+                      className="right-icons la la-file-pdf"
+                      aria-hidden="true"
+                    ></i>
+                  </a>
+                </li>
+                <li>
+                  <a
+                    className=" mr-2"
+                    href="#"
+                    id=""
+                    role="button"
+                    data-toggle=""
+                    aria-haspopup="true"
+                    aria-expanded="false"
+                  >
+                    <i
+                      className="right-icons la la-file-excel"
+                      aria-hidden="true"
+                    ></i>
+                  </a>
+                </li>
+                <li>
+                  <a
+                    className=" mr-2"
+                    href="#"
+                    id=""
+                    role="button"
+                    data-toggle=""
+                    aria-haspopup="true"
+                    aria-expanded="false"
+                  >
+                    <i
+                      className="right-icons la la-print"
+                      aria-hidden="true"
+                    ></i>
+                  </a>
+                </li>
+                <li>
+                  <a
+                    className=" mr-2"
+                    href="#"
+                    id=""
+                    role="button"
+                    data-toggle=""
+                    aria-haspopup="true"
+                    aria-expanded="false"
+                  >
+                    <i
+                      className="right-icons la la-star"
+                      aria-hidden="true"
+                    ></i>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </nav>
+          <div id="wrapper">
           <div className="leftmenu">
           <CompanyLeft />
           </div>
           <div className="contentbody">
           <CompanyContent />
+          </div>
           </div>
         </div>
       ),
@@ -41,6 +130,7 @@ const CompanySearch = () => {
     <>
       <Layout>
         <div className="searchtab-container">
+        <h3 className="card-body font-weight-bold">Search Companies</h3>
         <Tabs defaultActiveKey="1" items={items} onChange={onChange} type="card" />
         </div>
 {/*         
