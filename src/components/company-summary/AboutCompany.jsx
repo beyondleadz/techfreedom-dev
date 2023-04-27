17a64e018bab89d35cae3140fe9be21c2e74fbc7
unknown <naweditakeshari@gmail.com>
2023-04-27 15:34:18 +0530

    items added about company page and sign up flow corrected

--- src/components/company-summary/AboutCompany.jsx
+++ src/components/company-summary/AboutCompany.jsx
@@ -1,34 +1,36 @@
-import React,{useState} from "react";
-import {useSelector} from 'react-redux'
-import _ from 'lodash'
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import _ from "lodash";
 const AboutCompany = () => {
-  const [showMore, setShowMore] = useState(false);
+  const [showMoreProducts, setshowMoreProducts] = useState(false);
+  const [showMoreIntroduction, setshowMoreIntroduction] = useState(false);
   const companyDetails = useSelector(
     (state) => state.companyDetailsReducer.companyDetails
   );
 
-  const onClickShowMore = () => {
-    setShowMore(!showMore);
+  const onClickshowMoreProducts = () => {
+    setshowMoreProducts(!showMoreProducts);
   };
 
-//   const renderIndustry = (list,start,end) => {
-//     let industryList = _.cloneDeep(list);
-//     console.log(industryList,'sjfslkdfjl')
-//     // industryList = industryList?.splice(start,end)
-//     // return industryList.map((item) => {
-//     //     return <span>{item.name}, </span>
-//     // })
-//   }
+  const onClickShowMoreIntroduction =() => {
+    setshowMoreIntroduction(!showMoreIntroduction);
+  }
 
-  console.log(companyDetails,'sdkjfkls')
+  //   const renderIndustry = (list,start,end) => {
+  //     let industryList = _.cloneDeep(list);
+  //     console.log(industryList,'sjfslkdfjl')
+  //     // industryList = industryList?.splice(start,end)
+  //     // return industryList.map((item) => {
+  //     //     return <span>{item.name}, </span>
+  //     // })
+  //   }
+
+  console.log(companyDetails, "sdkjfkls");
 
   return (
     <div className="card shadow card-body">
       <div class="container">
         <div className="row mt-3 mb-2 w-100- fs-14">
-          <div className="col companyintro">
-            {companyDetails?.introduction}
-          </div>
           <div className="row mt-3 w-100- ">
             <div className="col w-col-3">
               <div>
@@ -36,7 +38,8 @@ const AboutCompany = () => {
                 <strong>Employees Range</strong>
               </div>
               <div className="gap-l">
-                {companyDetails?.totalEmployees} <span> on {companyDetails?.name} </span>
+                {companyDetails?.totalEmployees}{" "}
+                <span> on {companyDetails?.name} </span>
               </div>
             </div>
             <div className="col w-col-3">
@@ -71,24 +74,47 @@ const AboutCompany = () => {
                 <i className=" mr-2 text-black las la-cog"></i>
                 <span className="font-weight-bold">Technographics</span>
               </div>
-              <div className="gap-l">
-                JavaScript, HTML, PHP
-                <div>
-                  <strong>+1282 more (view full list) </strong>
-                </div>
-              </div>
+              <div className="gap-l">JavaScript, HTML, PHP</div>
             </div>
             <div className="col w-col-3">
+              <div>
+                <i className=" mr-2 text-black las la-cog"></i>
+                <span className="font-weight-bold">Updated on</span>
+              </div>
+              <div className="gap-l">Wednesday, April 262023</div>
+            </div>
+          </div>
+
+          <div className="row mt-3 w-100-">
+            <div className="col w-col-3" style={{ maxWidth: "33.3%" }}>
+              <div>
+                <i className=" mr-2 text-black las la-cog"></i>
+                <span className="font-weight-bold">
+                  Description of business
+                </span>
+              </div>
+              <div className="gap-l">
+                <div className={`contentcontainer ${showMoreIntroduction ? "" : "softexpand"}`}>
+                  {companyDetails?.introduction}
+                </div>
+                <button class="btn btn-light mr-2 " onClick={onClickShowMoreIntroduction}>
+                  {showMoreIntroduction ? "Hide.." : "More.."}
+                </button>
+              </div>
+            </div>
+
+            <div className="col w-col-3" style={{ maxWidth: "33.3%" }}>
               <div>
                 <i className=" text-black fs-16 las la-suitcase mr-2"></i>
                 <strong>Product and Services</strong>
               </div>
               <div className="gap-l">
-                <span>Retail, </span>
-                <span>Internet, </span>
-                <span>e-Commerce, </span>
-
-                <div className={showMore ? "" : "softexpand"}>
+                <div
+                  className={`contentcontainer ${showMoreProducts ? "" : "softexpand"}`}
+                >
+                  <span>Retail, </span>
+                  <span>Internet, </span>
+                  <span>e-Commerce, </span>
                   <span>Software, </span>
                   <span>Business Development, </span>
                   <span>Crowdsourcing, </span>
@@ -112,8 +138,8 @@ const AboutCompany = () => {
                   <span>Software Development, </span>
                   <span>Consumer Electronics &amp; Computers</span>
                 </div>
-                <button class="btn btn-light mr-2 " onClick={onClickShowMore}>
-                  {showMore ? "Hide.." : "More.."}
+                <button class="btn btn-light mr-2 " onClick={onClickshowMoreProducts}>
+                  {showMoreProducts ? "Hide.." : "More.."}
                 </button>
               </div>
             </div>
--- src/components/signup/Signup2.jsx
+++ src/components/signup/Signup2.jsx
@@ -1,60 +1,60 @@
-import React,{useState} from "react";
-import {formActionStep2} from '../../actionCreator/signUpActionCreater';
-import { useSelector,useDispatch } from "react-redux";
-const Step2=({changeStep})=>{
-	const [form,setForm]=useState({});
-	const [errorObj,setError] = useState()
-	const dispatch = useDispatch()
-	const OnInputChange=(ele)=>{
-		setForm({
-			...form,
-			[ele.target.name]:ele.target.value
-		})        
-	}
-	const sendData=()=>{
-		let errObj; let errCnt=false;
-		if(!form?.websiteUrl){
-			errCnt=true;
-			errObj={
-				...errObj,
-				websiteUrl:"Please enter website."
-			}
-		}
-		if(!form?.companyName){
-			errCnt=true;
-			errObj={
-				...errObj,
-				companyName:"Please enter company name."
-			}
-		}
-		setError(errObj);
-		if(!errCnt){
-		// const combinedData={...signupData,...form}
-		//console.log(signupData,form)
-		changeStep(3)
-		dispatch(formActionStep2(form))
-		}
-	}
-    return (
-        <>
-                        <h2>Step 2</h2>						
-						<form>
-							<div className="email">
-							<input placeholder="Website URL*" name="websiteUrl" type="text" required=""  value={form?.websiteUrl} onChange={OnInputChange}/>
-							<span className="icons i1"><i className="fa fa-user" aria-hidden="true"></i></span>
-							<div className="error">{errorObj?.websiteUrl}</div>
-							</div>
-							<div className="email">
-							<input placeholder="Company Name*" name="companyName" type="text" required="" value={form?.companyName} onChange={OnInputChange}/>
-							<span className="icons i1"><i className="fa fa-user" aria-hidden="true"></i></span>
-							<div className="error">{errorObj?.companyName}</div>
-							</div>
-							<input type="button" value="Save & Continue" name="register"  onClick={sendData}/>
-						</form>
+// import React,{useState} from "react";
+// import {formActionStep2} from '../../actionCreator/signUpActionCreater';
+// import { useSelector,useDispatch } from "react-redux";
+// const Step2=({changeStep})=>{
+// 	const [form,setForm]=useState({});
+// 	const [errorObj,setError] = useState()
+// 	const dispatch = useDispatch()
+// 	const OnInputChange=(ele)=>{
+// 		setForm({
+// 			...form,
+// 			[ele.target.name]:ele.target.value
+// 		})        
+// 	}
+// 	const sendData=()=>{
+// 		let errObj; let errCnt=false;
+// 		if(!form?.websiteUrl){
+// 			errCnt=true;
+// 			errObj={
+// 				...errObj,
+// 				websiteUrl:"Please enter website."
+// 			}
+// 		}
+// 		if(!form?.companyName){
+// 			errCnt=true;
+// 			errObj={
+// 				...errObj,
+// 				companyName:"Please enter company name."
+// 			}
+// 		}
+// 		setError(errObj);
+// 		if(!errCnt){
+// 		// const combinedData={...signupData,...form}
+// 		//console.log(signupData,form)
+// 		changeStep(3)
+// 		dispatch(formActionStep2(form))
+// 		}
+// 	}
+//     return (
+//         <>
+//                         <h2>Step 2</h2>						
+// 						<form>
+// 							<div className="email">
+// 							<input placeholder="Website URL*" name="websiteUrl" type="text" required=""  value={form?.websiteUrl} onChange={OnInputChange}/>
+// 							<span className="icons i1"><i className="fa fa-user" aria-hidden="true"></i></span>
+// 							<div className="error">{errorObj?.websiteUrl}</div>
+// 							</div>
+// 							<div className="email">
+// 							<input placeholder="Company Name*" name="companyName" type="text" required="" value={form?.companyName} onChange={OnInputChange}/>
+// 							<span className="icons i1"><i className="fa fa-user" aria-hidden="true"></i></span>
+// 							<div className="error">{errorObj?.companyName}</div>
+// 							</div>
+// 							<input type="button" value="Save & Continue" name="register"  onClick={sendData}/>
+// 						</form>
 												
-						<br/><br/><br/><br/>
-                        </>
-    )
-}
+// 						<br/><br/><br/><br/>
+//                         </>
+//     )
+// }
 
-export default Step2;
+// export default Step2;
