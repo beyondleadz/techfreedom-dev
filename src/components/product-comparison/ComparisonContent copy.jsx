b0ece086ba30a830df6bfa21e88a514d5dac4f85
jayanti <naweditakeshari@gmail.com>
2023-05-19 13:17:20 +0530

    page creation

--- src/components/product-comparison/ComparisonContent copy.jsx
+++ src/components/product-comparison/ComparisonContent copy.jsx
@@ -19,15 +19,15 @@ const ComparisonContent=()=>{
 				
 						 <li class="compare-navigation_item1 "> <img src={logo} />
 						 <div className="vs"> vs </div></li>
-							<li class="compare-navigation_item" aria-controls="tab_item-1" role="tab"><div><span>LinkedIn</span></div></li>
-							<li class="compare-navigation_item" aria-controls="tab_item-2" role="tab"> <div> <span>Traditional Data Companies</span></div></li>
-							<li class="compare-navigation_item" aria-controls="tab_item-3" role="tab"> <div><span>Popular Prospecting Tools</span></div></li>
-							<li class="compare-navigation_item" aria-controls="tab_item-4" role="tab"> <div> <span>New Age Marketing Tools</span></div></li></ul>
+							<li class="compare-navigation_item"><div><span>LinkedIn</span></div></li>
+							<li class="compare-navigation_item"> <div> <span>Traditional Data Companies</span></div></li>
+							<li class="compare-navigation_item"> <div><span>Popular Prospecting Tools</span></div></li>
+							<li class="compare-navigation_item"> <div> <span>New Age Marketing Tools</span></div></li></ul>
 				
 						<div className="clearfix"> </div>
 					</div>
 
-<div className="compare-table__table" aria-labelledby="tab_item-1">
+<div className="compare-table__table">
 <div className="compare-table__head">
 <div className="compare-table__col"></div>
 <div className="compare-table__col">Beyond Leads</div>
@@ -81,58 +81,6 @@ const ComparisonContent=()=>{
 </div>
 
 
-<div className="compare-table__table" aria-labelledby="tab_item-2">
-<div className="compare-table__head">
-<div className="compare-table__col"></div>
-<div className="compare-table__col">Beyond Leads</div>
-<div className="compare-table__col">Test 2</div>
-</div>
-<div className="compare-table__row">
-<div className="compare-table__col"></div>
-<div className="compare-table__col"><p className="compare-table__property">- Unlimited Prospecting and list builidng</p></div>
-<div className="compare-table__col"><p className="compare-table__property">- Prospecting is connections dependant</p></div>
-</div>
-<div className="compare-table__row">
-<div className="compare-table__col"></div>
-<div className="compare-table__col"><p className="compare-table__property">- Independent human verified data</p></div>
-<div className="compare-table__col"><p className="compare-table__property">- User Generated data</p></div>
-</div>
-<div className="compare-table__row">
-<div className="compare-table__col"></div>
-<div className="compare-table__col"><p className="compare-table__property">- Accurate and doubly verified data</p></div>
-<div className="compare-table__col"><p className="compare-table__property">- Accuracy and Completeness doubtful</p></div>
-</div>
-<div className="compare-table__row">
-<div className="compare-table__col"></div>
-<div className="compare-table__col"><p className="compare-table__property">- Multiple data soources with rigrouos quality contol</p></div>
-<div className="compare-table__col"><p className="compare-table__property">- Single Source (Users)</p></div>
-</div>
-<div className="compare-table__row">
-<div className="compare-table__col"></div>
-<div className="compare-table__col"><p className="compare-table__property">- Tailored dataset with multiple download options</p></div>
-<div className="compare-table__col"><p className="compare-table__property">- Limitations in customisng and downloding</p></div>
-</div>
-<div className="compare-table__row">
-<div className="compare-table__col"></div>
-<div className="compare-table__col"><p className="compare-table__property">- Budget Freindly and unlimited searches</p></div>
-<div className="compare-table__col"><p className="compare-table__property">-Limited free searches and Expensive per user billing</p></div>
-</div>
-<div className="compare-table__row">
-<div className="compare-table__col"></div>
-<div className="compare-table__col"><p className="compare-table__property">- Business emails</p></div>
-<div className="compare-table__col"><p className="compare-table__property">- Personal emails and costly reachout through Inmails</p></div>
-</div>
-<div className="compare-table__row">
-<div className="compare-table__col"></div>
-<div className="compare-table__col"><p className="compare-table__property">- Direct Dials & Mobile Numbers</p></div>
-<div className="compare-table__col"><p className="compare-table__property">- X</p></div>
-</div>
-<div className="compare-table__row">
-<div className="compare-table__col"></div>
-<div className="compare-table__col"><p className="compare-table__property">- No commercial advertisements and sponsored ads like LinkedIn</p></div>
-<div className="compare-table__col"><p className="compare-table__property">- Commercial and Sposorod ads take away attention</p></div>
-</div>
-</div>
 
 
 </div></div>
