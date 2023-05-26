7bbbdf93f09446130e344f087e76547e8938e173
nawedita <naweditakeshari@gmail.com>
2023-05-26 14:28:47 +0530

    update

--- src/components/executive-details/ExecutiveHeader.jsx
+++ src/components/executive-details/ExecutiveHeader.jsx
@@ -42,7 +42,7 @@ const ExecutiveHeader = () => {
   };
 
   return (
-    <div className="row col-md-12">
+    <div className="row pl-3 pr-3">
      <div className="headername">
         <div className="font-weight-bold mb-1 ">
           <h3>{executiveDetails?.fullname}</h3>
@@ -50,32 +50,32 @@ const ExecutiveHeader = () => {
       </div>
       <div>
       
-        <div className="card shadow card-body">
+        <div className=" card shadow card-body">
           <div className="executiveheader">
             <div className="executivelogobox">{executiveDetails?.firstname[0].toUpperCase()+executiveDetails?.lastname[0].toUpperCase()}</div>
 
             <div className="executivedescbox ">
               <div className="row mb-2">
                 <div className="col pl-4">
-                <div className="fs-12 font-weight-bold"><i class="text-black fs-16 la la-suitcase mr-2"></i>Designation  <div className="font-weight-normal ml-2">{executiveDetails?.title}</div></div>
+                <div className="fs-12 font-weight-bold">Designation  <div className="font-weight-normal">{executiveDetails?.title}</div></div>
                 </div>
                 <div className="col"></div>
                 <div className="col">
-                <div className="fs-12 font-weight-bold"><i class=" text-black fs-16 la la-building mr-2"></i>Company <div className="font-weight-normal ml-2">{executiveDetails?.company?.name}</div></div>
+                <div className="fs-12 font-weight-bold">Company <div className="font-weight-normal">{executiveDetails?.company?.name}</div></div>
                   
                 </div>
               </div>
               <div className="row pt-2 mb-2">
                 <div className="col">
-                <div className="fs-12 font-weight-bold"><i class=" text-black fs-16 la la-envelope mr-2"></i>Email</div>{" "}
+                <div className="fs-12 font-weight-bold">Email</div>{" "}
                   <i className=" align-top btn iconemail emails"></i>
                   <span>View Email</span>
                 </div>
                 <div className="col">
-                <div className="fs-12 font-weight-bold"><i class=" text-black la la-phone fs-16 mr-2"></i>Phone</div>{executiveDetails?.phoneNo}
+                <div className="fs-12 font-weight-bold">Phone</div>{executiveDetails?.phoneNo}
                 </div>
                 <div className="col">
-                <div className="fs-12 font-weight-bold"><i class=" text-black fs-16 la la-laptop mr-2"></i>Social</div>
+                <div className="fs-12 font-weight-bold">Social</div>
                   {/* <i class="lab fs-20 facebook lab la-facebook"></i>
                   <i class="lab fs-20  twitter la la-twitter-square"></i>
                   <i class="lab fs-20 linkedin lab la-linkedin"></i> */}
