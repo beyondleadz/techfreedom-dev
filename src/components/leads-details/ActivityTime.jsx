5d75c5ae7e3213c765dda6ca29637f0bfb88bcb7
nawedita <naweditakeshari@gmail.com>
2023-06-05 14:00:51 +0530

    update

--- src/components/leads-details/ActivityTime.jsx
+++ src/components/leads-details/ActivityTime.jsx
@@ -9,36 +9,36 @@ const ActivityTime = () => (
     items={[
       {
         color: 'green',
-        dot: <div><a href="#" class="btn btn-web btn-sm btn-circle"> <i class="las la-globe"></i></a></div>,
-        children: <div className="mt-3"><div>Website visit</div>
-        <p>http://beyondleads/leads</p>
-        <p>http://beyondleads/companyDetails</p>
+        dot: <div><a href="#" class="btn btn-phone btn-sm btn-circle"> <i class="las la-phone"></i></a></div>,
+        children: <div className="mt-3"><div> Phone Call</div>
+        <p>Called Main Office (open call)</p>
+        <p>called Jeff Mobile (open call)</p>
         </div>
         ,
       },
       {
         color: 'green',
-        dot: <div><a href="#" class="btn btn-phone btn-sm btn-circle"> <i class="las la-phone"></i></a></div>,
-        children: <div><div>Phone Call</div>
-        <p>called Main Office (open call)</p>
+        dot: <div><a href="#" class="btn btn-email btn-sm btn-circle"> <i class="las la-envelope"></i></a></div>,
+        children: <div><div>Email</div>
+        <p>called Main Office</p>
                 </div>,
       },
       {
         color: 'red',
-        dot: <div><a href="#" class="btn btn-app btn-sm btn-circle"> <i class="las la-phone"></i></a></div>,
+        dot: <div><a href="#" class="btn btn-whatsapp btn-sm btn-circle"> <i class="las la-mobile"></i></a></div>,
         children: (
          
-            <div><div>Phone Call</div>
-        <p>called Mike Office (open call)</p>
+            <div><div>Whatsapp</div>
+        <p> Mike Office</p>
                 </div>
          
         ),
       },
       {
-        dot: <div><a href="#" class="btn btn-email btn-sm btn-circle"> <i class="las la-envelope"></i></a></div>,
+        dot: <div><a href="#" class="btn btn-meet btn-sm btn-circle"> <i class="las la-handshake"></i></a></div>,
         children: (
           <>
-           <div><div>Completed Web Form</div>
+           <div><div>Meeting</div>
         <p>Small engine repair near me</p>
                 </div>
           </>
@@ -49,7 +49,7 @@ const ActivityTime = () => (
         dot: <div><a href="#" class="btn btn-act btn-sm btn-circle"> <i class="las la-phone"></i></a></div>,
         children: (
           <>
-            <div><div>Phone Call</div>
+            <div><div>Follow up</div>
         <p>called Jeff Mobile (open call)</p>
                 </div>
           </>
@@ -57,10 +57,10 @@ const ActivityTime = () => (
       },
       {
         color: 'gray',
-        dot: <div><a href="#" class="btn btn-doc btn-sm btn-circle"> <i class="las la-envelope"></i></a></div>,
+        dot: <div><a href="#" class="btn btn-app btn-sm btn-circle"> <i class="las la-mobile"></i></a></div>,
         children: (
           <>
-            <div><div>Sent Email</div>
+            <div><div>Chat</div>
         <p>Request Feedback</p>
                 </div>
           </>
@@ -68,7 +68,7 @@ const ActivityTime = () => (
       },
       {
         color: '#00CCFF',
-        dot: <div><a href="#" class="btn btn-phone btn-sm btn-circle"> <i class="las la-phone"></i></a></div>,
+        dot: <div><a href="#" class="btn btn-document btn-sm btn-circle"> <i class="las la-phone"></i></a></div>,
         children: <div><div>Review received</div>
         <p>Feedback received (Open Review)</p>
                 </div>,
