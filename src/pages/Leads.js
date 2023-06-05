2ec230f5bb8f8f9c4b2bed502cbc9d2341a8db98
nawedita <naweditakeshari@gmail.com>
2023-06-06 01:05:36 +0530

    update

--- src/pages/Leads.js
+++ src/pages/Leads.js
@@ -103,7 +103,7 @@ const Leads = () => {
                 </div></div>
                 <div className="col-md-8 col-custom-2">
                 <div className="row">
-                <Tabs className="ml-4 w-90" defaultActiveKey="1" items={items} onChange={onChange} />
+                <Tabs className="ml-4 " defaultActiveKey="1" items={items} onChange={onChange} />
                 </div>
                 </div></div></div></div></div></div>
 	 
