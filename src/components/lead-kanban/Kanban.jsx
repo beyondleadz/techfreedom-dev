60e89a02be892d63da8291ed25279e853997cad3
nawedita <naweditakeshari@gmail.com>
2023-07-23 19:32:29 +0530

    update

--- src/components/lead-kanban/Kanban.jsx
+++ src/components/lead-kanban/Kanban.jsx
@@ -5,15 +5,12 @@ import { Dropdown, Space, Button, menuProps } from 'antd'
 const items: MenuProps['items'] = [
     {
       key: '1',
-      label: 'Item 1',
+      label: 'Quick Create',
     },
-    {
-      key: '2',
-      label: 'Item 2',
-    },
+    
     {
       key: '3',
-      label: 'Item 3',
+      label: 'Import',
     },
   ];
   
@@ -36,7 +33,9 @@ const Kanban=()=>{
 
 <div id="kanban" className="container-fluid pt-3">
    
-<div className="col-sm-6 col-kanban col-xl-3">    <Dropdown
+<div className="card-header col-xl-12 col-lg-10 card  shadow col-kanban "> 
+<span className=" fs-14 ml-2 font-weight-bold text-primary mob-t">Showing 1-10 of 59 results</span>
+<span class="ml-4 fs-23 mr-3"><i class=" las la-calendar"></i><i className=" btn  mr-3 ml-3 kanbanlist"></i><i className=" btn  kanbanview"></i></span>   <Dropdown
     menu={{
       items,
       selectable: true,
@@ -45,13 +44,13 @@ const Kanban=()=>{
   >
     <Button>
       <Space>
-        Dropdown
+      <i class=" fs-14 font-weight-bold fa fa-plus"></i>Add Lead
         <DownOutlined />
       </Space>
     </Button>
   </Dropdown>
  
-    <span className="mr-4">   <Dropdown
+    {/* <span className="mr-4">   <Dropdown
     menu={{
       items,
       selectable: true,
@@ -60,14 +59,15 @@ const Kanban=()=>{
   >
     <Button>
       <Space>
-        Name
+        Action
         <DownOutlined />
       </Space>
     </Button>
   </Dropdown>
-  </span>
+  </span> */}
+  <span className="kanspan"><div class="buttons-container textsearch"><ul class="d-flex mt-1  m-mt"><li><a class=" mr-2" href="#" id="" role="button" data-toggle="" aria-haspopup="true" aria-expanded="false"><i class="right-icons las la-tags" aria-hidden="true"></i></a></li><li><a class=" mr-2" href="#" id="" role="button" data-toggle="" aria-haspopup="true" aria-expanded="false"><i class="right-icons la la-file-excel" aria-hidden="true"></i></a></li><li><a class=" mr-2" role="button" data-toggle="" aria-haspopup="true" aria-expanded="false"><i class="right-icons la la-file-pdf" aria-hidden="true"></i></a></li><li><a class=" mr-2" role="button" data-toggle="" aria-haspopup="true" aria-expanded="false"><i class="right-icons la la-print" aria-hidden="true"></i></a></li></ul><button type="button" class="ant-btn css-dev-only-do-not-override-1mqg3i0 ant-btn-default d-none d-sm-inline-block ml-2 btn-outline-grey"><i class="fas fa-bolt pr-1"></i><span> CONNECT TO CRM</span></button></div></span>
     </div>
-    <div className="col-sm-6 col-kanban col-xl-3"><i className=" btn mr-2  kanbanlist"></i><i className=" btn  kanbanview"></i></div>
+
     <h3 className="font-weight-light text-white">Kanban Board</h3>
      <div className=" main-kanban">
        
