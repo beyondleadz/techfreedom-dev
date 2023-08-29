f2bdcc8b2b8e7c502fdf8e306c983530f7c84c9e
nawedita <naweditakeshari@gmail.com>
2023-08-29 23:20:20 +0530

    update

--- src/components/dashboard/DashboardLead.jsx
+++ src/components/dashboard/DashboardLead.jsx
@@ -115,7 +115,7 @@ const DashboardLead=()=>{
 <div class="col-xl-3 col-md-6 mb-4">
 	<div class="card border-left-info shadow h-100 py-2">
 		<div class="card-body">
-		<div class="text-xs font-weight-bold text-info text-uppercase mb-1">Opportunity Won Ratio
+		<div class="text-xs font-weight-bold text-leftinfo text-uppercase mb-1">Opportunity Won Ratio
 					</div>
 			<div class="row no-gutters align-items-center">
 			
